22457b80b18c692807900e585eae055496bc00a6
Li Haotian <2878348186@qq.com>
2023-07-09 19:55:10 +0800

    add update.sh

---
+++ update.sh
@@ -0,0 +1,10 @@
+cp ~/.zshrc .
+cp ~/.vimrc .
+cp ~/.ideavimrc .
+cp ~/.gitconfig .
+cp -r ~/.config/alacritty ./.config
+cp -r ~/.config/mpv ./.config
+cp -r ~/.config/neofetch ./.config
+cp -r ~/.config/nvim ./.config
+cp -r ~/.config/ranger ./.config
+cp -r ~/.vim/autoload ./.vim
